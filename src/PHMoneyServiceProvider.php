365112fd613539728aa670670f58fb4e93e60122
Panayiotis Halouvas <phalouvas@kainotomo.com>
2022-05-17 17:22:19 +0300

    fix database config connection

--- src/PHMoneyServiceProvider.php
+++ src/PHMoneyServiceProvider.php
@@ -15,7 +15,7 @@ class PHMoneyServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/phmoney.php', 'phmoney');
-        $this->mergeConfigFrom(__DIR__.'/../config/database.php', 'database');
+        $this->mergeConfigFrom(__DIR__.'/../config/database.php', 'database.connections');
     }
 
     /**
