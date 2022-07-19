ca4d87f1002d66e438ef168a84ddbb5693224486
Panayiotis Halouvas <phalouvas@kainotomo.com>
2022-07-19 02:42:01 +0000

    fix phmoney_sqlite database name

--- config/database.php
+++ config/database.php
@@ -27,7 +27,7 @@ return [
     'phmoney_sqlite' => [
         'driver' => 'sqlite',
         'url' => env('DATABASE_URL_PORTFOLIO'),
-        'database' => env('DB_DATABASE', storage_path('app/import/sqlite/')),
+        'database' => env('DB_DATABASE_PORTFOLIO', storage_path('app/import/sqlite/')),
         'prefix' => '',
         'foreign_key_constraints' => env('DB_FOREIGN_KEYS_PORTFOLIO', true),
     ],
